8a7c87af14e8e9099cda1968ca11c2d7776dcc7f
script <anon42237@gmail.com>
2021-09-30 22:01:54 +0530

    fix path issue in installacation script

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root" 
+   echo "use command : sudo su"
    exit 1
 fi
 sudo apt-get update
 sudo apt-get install python3 python3-pip git uvicorn -y
 sudo git clone https://github.com/ScRiPt1337/spam_box /opt/
-pip install -r requirements.txt
+pip install -r /opt/hacksec_tempmail/requirements.txt
 cp ./hacksec_tempmail/service/* /etc/systemd/system/
 sudo systemctl enable hacksec_server.service
 sudo systemctl start hacksec_server.service
