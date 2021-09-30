e68395975c451d1bbcb09442fe4981567deaacb2
script <anon42237@gmail.com>
2021-09-30 21:53:34 +0530

    fix the install script

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 sudo apt-get update
 sudo apt-get install python3 python3-pip git uvicorn -y
+git clone https://github.com/ScRiPt1337/spam_box /opt/
 pip install -r requirements.txt
 cp ./hacksec_tempmail/service/* /etc/systemd/system/
 systemctl enable hacksec_server.service
