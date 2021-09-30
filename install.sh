3e4cb27ca8885277c1b280da9f82c82fcf752b99
script <anon42237@gmail.com>
2021-09-30 21:57:43 +0530

    fix the install script

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
 sudo apt-get update
 sudo apt-get install python3 python3-pip git uvicorn -y
-git clone https://github.com/ScRiPt1337/spam_box /opt/
+sudo git clone https://github.com/ScRiPt1337/spam_box /opt/
 pip install -r requirements.txt
 cp ./hacksec_tempmail/service/* /etc/systemd/system/
-systemctl enable hacksec_server.service
-systemctl start hacksec_server.service
-systemctl enable hacksec_mail.service
-systemctl start hacksec_mail.service
+sudo systemctl enable hacksec_server.service
+sudo systemctl start hacksec_server.service
+sudo systemctl enable hacksec_mail.service
+sudo systemctl start hacksec_mail.service
