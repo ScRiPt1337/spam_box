3bceeaf5b761385c0d89b99e5ba9c93a1568445f
ScRiPt1337 <anon42237@gmail.com>
2022-01-03 17:59:13 +0530

    fix mail body not visible

--- hacksec_tempmail/Mailbox.py
+++ hacksec_tempmail/Mailbox.py
@@ -1,5 +1,5 @@
 import os
-
+import email
 
 class Email:
     def __init__(self):
@@ -14,24 +14,12 @@ class Mail:
 
     @staticmethod
     def formate_email(data):
-        email = Email()
-        data = data.split("\n")
-        for i in data:
-            if i.startswith("From:"):
-                email.from_email = i.replace("From:", "")
-            elif i.startswith("Date:"):
-                email.date = i.replace("Date:", "")
-            elif i.startswith("Subject:"):
-                email.subject = i.replace("Subject:", "")
-            elif i.startswith("To:"):
-                email.to = i.replace("To:", "")
-            elif i.startswith('<div dir="ltr">'):
-                email.content = i
-            elif i.startswith('<div dir=3D"ltr">'):
-                count = data.index(i)
-                for i in data:
-                    if len(data) == count:
-                        break
-                    email.content += data[count]
-                    count += 1
-        return email
+        formated_email = Email()
+        msg = email.message_from_string(data)
+        formated_email.from_email = msg['from']
+        formated_email.date = msg["date"]
+        formated_email.subject = msg["subject"]
+        formated_email.to = msg["to"]
+        for payload in msg.get_payload():
+            formated_email.content += payload.get_payload()
+        return formated_email
